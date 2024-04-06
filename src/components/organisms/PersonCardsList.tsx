c51fdc0a8ad9e6952fceb4a8cc71ef0abf20c366
rita <rita.merhej@hotmail.com>
2024-04-06 11:07:22 +0300

    feat/ add load more and refresh

--- src/components/organisms/PersonCardsList.tsx
+++ src/components/organisms/PersonCardsList.tsx
@@ -4,14 +4,23 @@ import {User} from '../../data/data.types';
 
 type Props = {
   peopleList: User[];
+  isRefreshing?: boolean;
+  onRefresh?: () => void;
 };
 
-const PersonCardsList = ({peopleList}: Props) => {
+const PersonCardsList = ({peopleList, isRefreshing, onRefresh}: Props) => {
   const renderItem = () => {
     return <Text>Hello</Text>;
   };
   if (peopleList.length > 0) {
-    return <FlatList data={peopleList} renderItem={renderItem} />;
+    return (
+      <FlatList
+        data={peopleList}
+        renderItem={renderItem}
+        refreshing={isRefreshing}
+        onRefresh={onRefresh}
+      />
+    );
   } else {
     return <Text>No cat parents registered yet!</Text>;
   }
--- src/screens/OtherUsersScreen.tsx
+++ src/screens/OtherUsersScreen.tsx
@@ -2,28 +2,35 @@ import {SafeAreaView} from 'react-native';
 import React from 'react';
 import PersonCardsList from '../components/organisms/PersonCardsList';
 import useManageUsersFetching from '../hooks/useManageUsersFetching';
+import {USERS_LIMIT} from '../service/api.data';
+import LoadMoreButton from '../components/atoms/Buttons/LoadMoreButton';
 
 const OtherUsersScreen = () => {
   const {
     allUsers,
-    // endReached,
-    // isLoading,
-    // refresh,
-    // handleLoadMore,
-    // handleRefresh,
+    endReached,
+    isLoading,
+    refresh,
+    handleLoadMore,
+    handleRefresh,
   } = useManageUsersFetching();
 
-  // const pageToFetch =
-  //   allUsers.length >= 3 ? allUsers.length / USERS_LIMIT + 1 : 1;
+  const pageToFetch = JSON.stringify(
+    allUsers.length >= 3 ? allUsers.length / USERS_LIMIT + 1 : 1,
+  );
 
   return (
     <SafeAreaView>
-      <PersonCardsList peopleList={allUsers} />
-      {/* <LoadMoreButton
-        onPress={handleLoadMore}
+      <PersonCardsList
+        peopleList={allUsers}
+        isRefreshing={refresh}
+        onRefresh={handleRefresh}
+      />
+      <LoadMoreButton
+        onPress={handleLoadMore(pageToFetch)}
         isLoading={isLoading}
         endReached={endReached}
-      /> */}
+      />
     </SafeAreaView>
   );
 };
